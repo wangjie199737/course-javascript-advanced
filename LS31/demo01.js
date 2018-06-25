43e0b1e10e6a8ed5982a86acf41ab48e3aa48ced
wangjie199737 <419009594@>
2018-06-25 15:58:54 +0800

    update

--- LS31/demo01.js
+++ LS31/demo01.js
@@ -45,6 +45,8 @@ for (var i of s2) {
 }// 2 3 5 4
 var items = new Set([1, 2, 3, 4, 5, 5, 5, 5]);
 console.log(items.size); // 5
+//WeakSet 保证内存利用率
+
 //3.map
 let map = new Map();
 map.set(1, 'aaa').set(1, 'bbb');
@@ -57,3 +59,56 @@ map.set(k1, 111);
 map.set(k2, 222);
 map.get(k1); // 111
 map.get(k2); // 222
+
+var map = new Map();
+var k1 = 'a';
+var k2 = 'a';
+map.set(k1, 111);
+map.set(k2, 222);
+map.get(k1); // 222
+map.get(k2); // 222
+//如果是一样的类型 也就是三等的关系 那么就会覆盖  但是如果是不等的数据类型 就不会覆盖
+
+
+//1.数据结构（key可以是任意数据类型 节点等等）
+var map=new Map();
+map.set(['a'],555);            //map在set和get时候是严格判等的 ===
+console.log(map.get(['a']));
+//undefined
+var map=new Map();
+map.set('a',555);           
+console.log(map.get('a'));
+//555
+var map=new Map();
+var p1=['a'];
+map.set(p1,555);           
+console.log(map.get(p1));
+//555
+
+//注意下解构赋值就是在遍历的时候？？？？？？？
+//filter 过滤  =>并不是箭头函数而是变量和值的关系
+
+//下一章：
+// this的指向
+// 如果是一个普通函数就是指向window 如果是用new就是指向这个对象 
+var p1=new Number();
+p1.__proto__==Number.prototype;//true
+p1.__proto__===Number.prototype;//true
+//实例化的对象的链子 还有就是构造器的链子
+//如何访问私有属性？
+var Point=function(x,y,z){
+    this.x=x;
+    this.y=y;
+    var z=z;//私有属性
+}
+var p=new Point(1,2,3);
+Point.prototype.show=function(){
+    console.log(x,y,z);
+}
+//私有变量不能通过p.z来访问，而只能是通过函数来访问z。而且通过函数访问时候必须是z 而不是this.z 其他的是this.x和this.y
+
+//里面的属性默认是不可枚举 
+
+//属性三种：定义的、set和get、内部的
+
+//new命令 
